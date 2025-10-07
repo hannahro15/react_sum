d502bc5166118015b6cb66ce76a3a246ad3afb69
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-10-07 16:37:34 +0100

    Solution

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,13 @@
-// export const Sum = () => ();
+import React from 'react';
+
+export const Sum = () => (
+  <>
+    <p>Sum of 2 and 3 is 5</p>
+    <p>Sum of -5 and 5 is 0</p>
+    <p>Sum of 10 and 0 is 10</p>
+    <p>Sum of 0 and 5 is 5</p>
+    <p>Sum of 0 and 0 is 0</p>
+    <p>Sum of 6 and 0 is 6 </p>
+    <p>Sum of 0 and 12 is 12</p>
+  </>
+);
